e3ba19779e3478fff05992dbaeae3956093571be
Vitor Fonseca <vitorlofonseca@hotmail.com>
2023-08-10 10:47:38 -0300

    Configuring docker to run Grafana

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5175
+  },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
