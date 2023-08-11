5bcc9b9809c45c3828448bfd9232b5d7a4b067c6
Vitor Fonseca <vitorlofonseca@hotmail.com>
2023-08-11 18:16:56 -0300

    Configuring Prometheus web UI, and taking out CORS

--- backend/main.js
+++ backend/main.js
@@ -8,18 +8,9 @@ import cors from "cors";
 const app = express();
 const port = 3001;
 
-const allowedOrigins = ["http://localhost:5174"];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*", //THIS IS JUST A DEMO, DONT DO THIS IN PRODUCTION
   })
 );
 
