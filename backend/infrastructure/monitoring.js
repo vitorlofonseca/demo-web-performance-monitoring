1a06f0836e919ed60a55cc165f5479949e9511c9
Vitor Fonseca <vitorlofonseca@hotmail.com>
2023-08-28 16:25:05 +0100

    Drying project just to consider web-vitals metrics

--- backend/infrastructure/monitoring.js
+++ backend/infrastructure/monitoring.js
@@ -1,30 +1,13 @@
 import Prometheus from "prom-client";
 import "json-circular-stringify";
 
-const prometheusMetrics = [];
-
-const getPrometheusMetricByIdentifier = (identifier) => {
-  if (!prometheusMetrics[identifier]) {
-    prometheusMetrics[identifier] = new Prometheus.Histogram({
-      name: `${identifier}_duration_ms`,
-      help: `Duration of request to ${identifier} in ms`,
-      labelNames: ["route"],
-      buckets: [
-        0.1, 5, 15, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-      ],
-    });
-  }
-
-  return prometheusMetrics[identifier];
-};
-
 const fidMetricScore = new Prometheus.Histogram({
   name: "fid_metric_score",
   help: "Score of web vitals FID metric",
   labelNames: ["metric"],
   buckets: [
     0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10,
+    10, 100, 200,
   ],
 });
 
@@ -34,7 +17,7 @@ const clsMetricScore = new Prometheus.Histogram({
   labelNames: ["metric"],
   buckets: [
     0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10,
+    10, 100, 200,
   ],
 });
 
@@ -44,35 +27,11 @@ const lcpMetricScore = new Prometheus.Histogram({
   labelNames: ["metric"],
   buckets: [
     0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10,
+    10, 100, 200,
   ],
 });
 
 const webVitalsMetrics = { fidMetricScore, clsMetricScore, lcpMetricScore };
-
-export const trackRequestTimeMiddleware = (req, res, next) => {
-  const startTimeRequest = Date.now();
-  const oldSend = res.send;
-
-  res.send = function (data) {
-    const endTimeRequest = Date.now();
-
-    console.log(
-      `The request time over ${req.originalUrl} at ${new Date()} has taken ${
-        endTimeRequest - startTimeRequest
-      }ms`
-    );
-
-    getPrometheusMetricByIdentifier(req.originalUrl).observe(
-      endTimeRequest - startTimeRequest
-    );
-
-    res.send = oldSend;
-    return res.send(data);
-  };
-
-  next();
-};
 
 export const pushFrontendMetric = async (req, res) => {
   const body = req.body;
--- backend/main.js
+++ backend/main.js
@@ -17,10 +17,6 @@ app.use(
 app.use(
   promMid({
     metricsPath: "/metrics",
-    collectDefaultMetrics: true,
-    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
   })
 );
 
