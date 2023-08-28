36fb6fe4118519efb1b1c01dca5f30c3bb2045f3
Vitor Fonseca <vitorlofonseca@hotmail.com>
2023-08-28 17:13:23 +0100

    Creating new metrics to turn easier to track high values

--- backend/infrastructure/monitoring.js
+++ backend/infrastructure/monitoring.js
@@ -1,46 +1,66 @@
 import Prometheus from "prom-client";
 import "json-circular-stringify";
 
+const HIGH_SCORE_LIMIT_WEB_VITALS = 1;
+
 const fidMetricScore = new Prometheus.Histogram({
   name: "fid_metric_score",
   help: "Score of web vitals FID metric",
   labelNames: ["metric"],
-  buckets: [
-    0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10, 100, 200,
-  ],
+  buckets: [0, 0.2, 0.5, 0.7, 1],
 });
 
 const clsMetricScore = new Prometheus.Histogram({
   name: "cls_metric_score",
   help: "Score of web vitals CLS metric",
   labelNames: ["metric"],
-  buckets: [
-    0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10, 100, 200,
-  ],
+  buckets: [0, 0.2, 0.5, 0.7, 1],
 });
 
 const lcpMetricScore = new Prometheus.Histogram({
   name: "lcp_metric_score",
   help: "Score of web vitals LCP metric",
   labelNames: ["metric"],
-  buckets: [
-    0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5,
-    10, 100, 200,
-  ],
+  buckets: [0, 0.2, 0.5, 0.7, 1],
 });
 
-const webVitalsMetrics = { fidMetricScore, clsMetricScore, lcpMetricScore };
+const quantityOfHighLCP = new Prometheus.Counter({
+  name: "quantity_high_lcp",
+  help: "Counter of accesses with high LCP metric",
+});
+
+const quantityOfHighFID = new Prometheus.Counter({
+  name: "quantity_high_fid",
+  help: "Counter of accesses with high FID metric",
+});
+
+const quantityOfHighCLS = new Prometheus.Counter({
+  name: "quantity_high_cls",
+  help: "Counter of accesses with high CLS metric",
+});
+
+const webVitalsMetrics = {
+  fidMetricScore,
+  clsMetricScore,
+  lcpMetricScore,
+  quantityOfHighLCP,
+  quantityOfHighFID,
+  quantityOfHighCLS,
+};
 
 export const pushFrontendMetric = async (req, res) => {
   const body = req.body;
 
   console.log(`Value ${body.value} received for metric ${body.name}`);
 
-  const prometheusMetricName = `${body.name.toLowerCase()}MetricScore`;
+  const webVitalBucketName = `${body.name.toLowerCase()}MetricScore`;
 
-  webVitalsMetrics[prometheusMetricName].labels(body.name).observe(body.value);
+  webVitalsMetrics[webVitalBucketName].labels(body.name).observe(body.value);
+
+  if (body.value > HIGH_SCORE_LIMIT_WEB_VITALS) {
+    const highMetricBucketName = `quantityOfHigh${body.name}`;
+    webVitalsMetrics[highMetricBucketName].inc();
+  }
 
   res.send();
 };
--- frontend/src/frontend-monitoring.ts
+++ frontend/src/frontend-monitoring.ts
@@ -2,13 +2,16 @@ import { onLCP, onFID, onCLS } from 'web-vitals'
 import { post } from './services/http'
 
 onCLS(async (clsInformation) => {
+  console.info('CLS metric information', clsInformation)
   post('/push-frontend-metric', clsInformation)
 })
 
 onFID(async (fidInformation) => {
+  console.info('FID metric information', fidInformation)
   post('/push-frontend-metric', fidInformation)
 })
 
 onLCP((lcpInformation) => {
+  console.info('LCP metric information', lcpInformation)
   post('/push-frontend-metric', lcpInformation)
 })
